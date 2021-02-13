37df68ee003f51fdf80efc51d80054fcd4cc1f90
Luiz Junior <lsjunior.it@gmail.com>
2021-02-13 20:56:43 -0300

    criação do componente pautaLista

---
+++ src/app/pauta/pauta-lista/pauta-lista.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PautaListaComponent } from './pauta-lista.component';
+
+describe('PautaListaComponent', () => {
+  let component: PautaListaComponent;
+  let fixture: ComponentFixture<PautaListaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PautaListaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PautaListaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pauta/pauta-lista/pauta-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pauta-lista',
+  templateUrl: './pauta-lista.component.html',
+  styleUrls: ['./pauta-lista.component.css']
+})
+export class PautaListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
