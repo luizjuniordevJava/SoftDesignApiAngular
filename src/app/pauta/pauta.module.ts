c59db12afec1e92352ae8b30927a18e252d59f6c
Luiz Junior <lsjunior.it@gmail.com>
2021-02-13 20:50:44 -0300

    criação do modulo pauta

---
+++ src/app/pauta/pauta.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PautaRoutingModule } from './pauta-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PautaRoutingModule
+  ]
+})
+export class PautaModule { }
