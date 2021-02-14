ee37d606ad4803a451f65179eb7b01dce1ed004b
Luiz Junior <lsjunior.it@gmail.com>
2021-02-13 22:08:31 -0300

    Implementação das rotas

--- src/app/pauta/pauta-routing.module.ts
+++ src/app/pauta/pauta-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PautaFormComponent } from './pauta-form/pauta-form.component';
+import { PautaListaComponent } from './pauta-lista/pauta-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'associado-form', component: PautaFormComponent},
+  { path: 'associado-form/:id', component: PautaFormComponent},
+  { path: 'associado-lista', component: PautaListaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
