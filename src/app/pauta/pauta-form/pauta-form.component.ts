2c137d687fc34e201aff06e9d271f30d2084de11
Luiz Junior <lsjunior.it@gmail.com>
2021-02-13 21:24:21 -0300

    Implementação dos metodos para serem utilizado no componente pautaForm

--- src/app/pauta/pauta-form/pauta-form.component.ts
+++ src/app/pauta/pauta-form/pauta-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { PautaService } from 'src/app/pauta.service';
+import { Pauta } from '../pauta';
+import { PautaModule } from '../pauta.module';
 
 @Component({
   selector: 'app-pauta-form',
@@ -7,9 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PautaFormComponent implements OnInit {
 
-  constructor() { }
+  pauta: Pauta;
+  sucesso: boolean = false;
+  errors: String[];
+  id: number;
+
+
+  constructor(
+    private service: PautaService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { 
+    this.pauta = new Pauta();
+  }
 
   ngOnInit(): void {
+    let params: Observable<Params> = this.activatedRoute.params;
+    params.subscribe(
+      urlParams => {
+        this.id = urlParams['id'];
+        if( this.id ){
+          this.service
+            .getPautaById(this.id)
+            .subscribe(
+              response => this.pauta = response,
+              errorResponse => this.pauta = new Pauta()
+            )
+        }
+      }
+    )
+  }
+
+  voltarParaListagem(){
+    this.router.navigate(['/pauta-lista']);
+  }
+
+  onSubmit(){
+    if( this.id ){
+
+      this.service
+            .atualizar(this.pauta)
+            .subscribe( response =>{
+              this.sucesso = true;
+              this.errors = null;
+            }, errorResponse =>{
+              this.errors = ['Erro ao atualizar a Pauta.']
+            })
+
+    }else{
+
+      this.service
+      .salvar(this.pauta)
+      .subscribe( response =>{
+        this.sucesso = true;
+        this.errors = null;
+        this.pauta = response;
+      }, errorResponse =>{
+        //console.log(errorResponse.error.errors);
+        this.errors = errorResponse.error.errors;
+        this.sucesso = false;
+      })
+    }
   }
 
 }
