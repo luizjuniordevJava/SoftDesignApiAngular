bbff1bf5d4ce2351c89c20aa02450f69ae50a3ef
Luiz Junior <lsjunior.it@gmail.com>
2021-02-15 18:48:51 -0300

    Implementação do componente de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  username: string;
+  password: string;
+  loginError: boolean;
+  cadastrando: boolean;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) { }
+
+  onSubmit(){
+    console.log(`User: ${this.username}, Pass: ${this.password}`);
+    this.router.navigate(['/home'])
+    
+  }
+
+  preparaCadastro(event){
+    event.preventDefault();
+    this.cadastrando = true;
+  }
+
+  cancelaCadastro(){
+    this.cadastrando = false;
   }
 
 }
