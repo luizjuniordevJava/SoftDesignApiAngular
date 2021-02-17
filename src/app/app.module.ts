fe7ec023581af6750130f6a5094dc9671d4f7a39
Luiz Junior <lsjunior.it@gmail.com>
2021-02-16 21:43:32 -0300

    Injeção da depedência do serviço auth.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PautaService } from './pauta.service';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { LayoutComponent } from './layout/layout.component';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   providers: [
     AssociadoService,
-    PautaService
+    PautaService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
